e4bfb9f42e025fd5b5049198da13608435b63075
dodm <centropatiobackup@gmail.com>
2019-03-25 09:54:34 -0300

    Alterações em Controlador

--- src/exercicio6/Controlador.java
+++ src/exercicio6/Controlador.java
@@ -1,7 +1,6 @@
 package exercicio6;
 
 import java.util.ArrayList;
-import java.util.Iterator;
 import java.util.List;
 import java.util.Scanner;
 
@@ -35,43 +34,65 @@ public class Controlador {
 		if (this.getCodigoDeSeguranca().equals(result)) {
 			//Criando lista de sensores
 			sensores = new ArrayList<Sensor>();
-			Iterator<Sensor> itr = sensores.iterator();
 			
 			System.out.println("Login realizado com sucesso.");
 			int opcao = 0;
-			while(opcao != 4) {	
+			while(opcao != 19203) {	
 				System.out.println("Opcoes: ");
-				System.out.println("\t1 - Adicionar novo sensor:\n\t2 - Acionar sensor(aciona o sensor):\n\t3 - Ativar sensor(altera o seu status para ativo):\n\t4 - Finalizar sessao");
+				System.out.println("\t1 - Adicionar novo sensor:\n\t2 - Ativar sensor(altera o seu status para ativo):\n\t3 - Acionar sensor(aciona o sensor):\n\t4 - Finalizar sessao");
 				opcao = entrada.nextInt();
 				switch (opcao) {
 					//Adicionar sensor
 					case 1:
-						System.out.println("Digite o nome do sensor: ");
+						System.out.println("Digite o nome do sensor: ");						
 						Scanner nomeSensor = new Scanner(System.in);
-						System.out.println(nomeSensor.nextLine());
-						Sensor sensor = new Sensor(nomeSensor.nextLine());
-						sensores.add(sensor);						
-					break;
-					
-					//Acionar sensor
-					case 2:
-//						System.out.println("Verificar sensor");
-//						System.out.println("Digite o nome do sensor: ");
-//						Scanner nomeSensorVerificacao = new Scanner(System.in);
-						System.out.println("Sensores disponíveis: ");
-						while(itr.hasNext()) {
-							Object sensores = itr.next();
-							System.out.print(sensores + " ");
-						}
-						System.out.println();
+						String id = nomeSensor.nextLine();
+						Sensor sensor = new Sensor(id);
+						sensores.add(sensor);
 					break;
 					
 					//Ativar sensor
-					case 3:
+					case 2:
+						System.out.println("Sensores disponíveis: ");
+						for(Sensor sensor1 : sensores) {
+							System.out.print(sensor1.getId()+" ");
+						}
+						System.out.println();
+						System.out.println("Digite o nome do sensor a ser ativado: ");			
+						Scanner sensorASerPesquisado = new Scanner(System.in);
+						String respostaAtivacao = sensorASerPesquisado.nextLine();
+						for(Sensor sensorPesquisa : sensores) {
+							if(sensorPesquisa.getId().equals(respostaAtivacao)) {
+								sensorPesquisa.setAtivo(true);
+							}
+						}
 					break;
 					
-					//Finalizar sessao
+					//Acionar sensor
+					case 3:
+						System.out.println("Sensores disponíveis: ");
+						for(Sensor sensor1 : sensores) {
+							System.out.print(sensor1.getId()+" ");
+						}
+						System.out.println();
+						System.out.println("Digite o nome do sensor a ser ativado: ");
+						Scanner sensorASerAcionado = new Scanner(System.in);
+						String respostaAcionamento = sensorASerAcionado.nextLine();
+						for(Sensor sensorPesquisa : sensores) {
+							if(sensorPesquisa.getId().equals(respostaAcionamento) && sensorPesquisa.isAtivo()) {
+								sensorPesquisa.setStatus("acionado");
+							}
+							else System.out.println("Falha no acionamento.");
+						}
+					break;
+					
+					//Desligar sistema
 					case 4:
+						System.out.println("Digite o codigo de seguranca: ");
+						Scanner scanner = new Scanner(System.in);
+						String senhaDesligar = scanner.nextLine();
+						boolean resultado = this.desligarSistema(senhaDesligar);
+						if(resultado) return;
 					break;
 					
 					default: 
@@ -84,7 +105,20 @@ public class Controlador {
 		
 	}
 	
-	public void desligarSistema() {}
+	public boolean desligarSistema(String codigoDeSeguranca) {
+		if(codigoDeSeguranca.equals(this.codigoDeSeguranca)) {
+			System.out.println("Sessão finalizada.");
+			return true;
+		}
+		else {
+			System.out.println("Senha incorreta.");
+			return false;
+		}
+	}
+	
+	public void verificaStatus() {
+		
+	}
 	
 	public String getCodigoDeSeguranca() {
 		return this.codigoDeSeguranca;
